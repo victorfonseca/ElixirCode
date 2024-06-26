4c552905bf67a4ae7f398e511d1b64bdb469b14d
Victor Fonseca <victortales@gmail.com>
2024-06-26 17:59:10 +0000

    Cleaning the devcontainer

--- hello_world/lib/hello_world/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule HelloWorld.Mailer do
-  use Swoosh.Mailer, otp_app: :hello_world
-end
--- hello_world/lib/hello_world/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule HelloWorld.Repo do
-  use Ecto.Repo,
-    otp_app: :hello_world,
-    adapter: Ecto.Adapters.Postgres
-end
