946f4c7973307bb7c3d98f34989d68b6c3598e2a
Victor Fonseca <victortales@gmail.com>
2024-06-28 17:43:03 +0000

    Several Improvements for State File Management

--- advanced_kv_store/lib/advance_kv_store/gen_server_store.ex
+++ advanced_kv_store/lib/advance_kv_store/gen_server_store.ex
@@ -2,15 +2,20 @@ defmodule AdvancedKvStore.GenServerStore do
   use GenServer
   require Logger
 
-  @persistence_file "kv_store.dat"
+  @default_persistence_file "kv_store.dat"
   @save_interval :timer.minutes(1)
-
-  defp calculate_expiry(:infinity), do: :infinity
-  defp calculate_expiry(ttl), do: System.system_time(:millisecond) + ttl
 
   # Client API
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def get_persistence_file do
+    GenServer.call(__MODULE__, :get_persistence_file)
+  end
+
+  def set_persistence_file(new_file) do
+    GenServer.call(__MODULE__, {:set_persistence_file, new_file})
   end
 
   def set(pid, key, value, ttl \\ :infinity) do
@@ -37,66 +42,23 @@ defmodule AdvancedKvStore.GenServerStore do
     GenServer.call(pid, :list_values)
   end
 
-  defp save_state(state) do
-    binary = :erlang.term_to_binary(state.data)
-    File.write!(@persistence_file, binary)
-  end
-
-  defp load_state do
-    case File.read(@persistence_file) do
-      {:ok, binary} ->
-        data = :erlang.binary_to_term(binary)
-        %{data: data, timers: %{}}
-      {:error, :enoent} ->
-        %{data: %{}, timers: %{}}
-    end
-  end
-
   # Server Callbacks
   @impl true
   def init(:ok) do
     Logger.debug("GenServerStore initialized")
-    state = load_state()
+    state = load_state(@default_persistence_file)
     schedule_save()
-    {:ok, state}
+    {:ok, Map.put(state, :persistence_file, @default_persistence_file)}
   end
 
   @impl true
-  def handle_info(:save, state) do
-    Logger.debug("Saving state to disk")
-    save_state(state)
-    schedule_save()
-    {:noreply, state}
+  def handle_call(:get_persistence_file, _from, state) do
+    {:reply, state.persistence_file, state}
   end
 
   @impl true
-  def handle_info({:check_expiry, key}, state) do
-    Logger.debug("Checking expiry for #{inspect(key)}")
-    case Map.get(state.data, key) do
-      nil ->
-        Logger.debug("#{inspect(key)} not found")
-        {:noreply, state}
-      {_value, expiry} ->
-        current_time = System.system_time(:millisecond)
-        if expiry == :infinity or expiry > current_time do
-          Logger.debug("#{inspect(key)} not expired yet")
-          time_left = max(0, expiry - current_time)
-          new_timer = Process.send_after(self(), {:check_expiry, key}, time_left)
-          new_timers = Map.put(state.timers, key, new_timer)
-          new_state = %{state | timers: new_timers}
-          {:noreply, new_state}
-        else
-          Logger.debug("#{inspect(key)} expired and removed")
-          new_data = Map.delete(state.data, key)
-          new_timers = Map.delete(state.timers, key)
-          new_state = %{state | data: new_data, timers: new_timers}
-          {:noreply, new_state}
-        end
-    end
-  end
-
-  defp schedule_save do
-    Process.send_after(self(), :save, @save_interval)
+  def handle_call({:set_persistence_file, new_file}, _from, state) do
+    {:reply, :ok, %{state | persistence_file: new_file}}
   end
 
   @impl true
@@ -173,6 +135,7 @@ defmodule AdvancedKvStore.GenServerStore do
       nil ->
         Logger.debug("#{inspect(key)} not found")
         {:reply, nil, state}
+
       {value, expiry} ->
         current_time = System.system_time(:millisecond)
         if expiry == :infinity or expiry > current_time do
@@ -208,17 +171,114 @@ defmodule AdvancedKvStore.GenServerStore do
   @impl true
   def handle_call(:list_values, _from, state) do
     Logger.debug("Listing all values")
-    values = state.data
-    |> Map.values()
-    |> Enum.map(fn {value, expiry} ->
-      current_time = System.system_time(:millisecond)
-      if expiry == :infinity or expiry > current_time do
-        value
-      else
-        nil
-      end
-    end)
-    |> Enum.reject(&is_nil/1)
+    values =
+      state.data
+      |> Map.values()
+      |> Enum.map(fn {value, expiry} ->
+        current_time = System.system_time(:millisecond)
+        if expiry == :infinity or expiry > current_time do
+          value
+        else
+          nil
+        end
+      end)
+      |> Enum.reject(&is_nil/1)
+
     {:reply, values, state}
+  end
+
+  @impl true
+  def handle_info(:save, state) do
+    Logger.debug("Saving state to disk")
+    save_state(state)
+    schedule_save()
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_info({:check_expiry, key}, state) do
+    Logger.debug("Checking expiry for #{inspect(key)}")
+    case Map.get(state.data, key) do
+      nil ->
+        Logger.debug("#{inspect(key)} not found")
+        {:noreply, state}
+
+      {_value, expiry} ->
+        current_time = System.system_time(:millisecond)
+        if expiry == :infinity or expiry > current_time do
+          Logger.debug("#{inspect(key)} not expired yet")
+          time_left = max(0, expiry - current_time)
+          new_timer = Process.send_after(self(), {:check_expiry, key}, time_left)
+          new_timers = Map.put(state.timers, key, new_timer)
+          new_state = %{state | timers: new_timers}
+          {:noreply, new_state}
+        else
+          Logger.debug("#{inspect(key)} expired and removed")
+          new_data = Map.delete(state.data, key)
+          new_timers = Map.delete(state.timers, key)
+          new_state = %{state | data: new_data, timers: new_timers}
+          {:noreply, new_state}
+        end
+    end
+  end
+
+  # Private functions
+  defp calculate_expiry(:infinity), do: :infinity
+  defp calculate_expiry(ttl), do: System.system_time(:millisecond) + ttl
+
+  defp blank?(data) when is_map(data), do: Enum.empty?(data)
+  defp blank?(str_or_nil), do: "" == str_or_nil |> to_string() |> String.trim()
+
+  defp save_state(state) do
+    case blank?(state.data) do
+      true ->
+        Logger.warning("No data to save")
+        :ok
+
+      false ->
+        binary = :erlang.term_to_binary(state.data)
+        compressed = :zlib.compress(binary)
+        case File.write(state.persistence_file, compressed) do
+          :ok ->
+            Logger.debug("State saved successfully")
+            :ok
+
+          {:error, reason} ->
+            Logger.error("Failed to save state: #{inspect(reason)}")
+            {:error, reason}
+        end
+    end
+  end
+
+  defp load_state(persistence_file) do
+    case File.read(persistence_file) do
+      {:ok, compressed} ->
+        binary = :zlib.uncompress(compressed)
+        data = :erlang.binary_to_term(binary)
+        current_time = System.system_time(:millisecond)
+
+        valid_data = Enum.reduce(data, %{}, fn {key, {value, expiry}}, acc ->
+          if expiry == :infinity or expiry > current_time do
+            Map.put(acc, key, {value, expiry})
+          else
+            Logger.debug("Expired key removed during load: #{inspect(key)}")
+            acc
+          end
+        end)
+
+        %{data: valid_data, timers: %{}}
+
+      {:error, :enoent} ->
+        Logger.info("No existing state file found. Starting with empty state.")
+        %{data: %{}, timers: %{}}
+
+      {:error, reason} ->
+        Logger.error("Failed to load state: #{inspect(reason)}")
+        %{data: %{}, timers: %{}}
+    end
+  end
+
+  defp schedule_save do
+    Process.send_after(self(), :save, @save_interval)
   end
 end
